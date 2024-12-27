8e2cb931833db4138d1d7bd15b0d65e456401079
Tim Baz <timtb.dev@gmail.com>
2024-12-26 19:26:20 -0800

    refactor ContactPage component: update logo size, improve layout spacing, and replace button with link for social items

--- app/(pages)/contact/page.tsx
+++ app/(pages)/contact/page.tsx
@@ -7,14 +7,14 @@ import Image from "next/image";
 export default async function ContactPage() {
   return (
     <Card>
-      <div className="mx-auto max-w-md sm:max-w-3xl">
+      <div className="mx-auto max-w-md py-2 sm:max-w-xl sm:py-4">
         <div>
           <div className="text-center">
             <Image
               src={logo}
               alt="Logo"
-              width={48}
-              height={48}
+              width={96}
+              height={96}
               className="mx-auto rounded-full object-cover"
             />
             <div className="mt-2">
@@ -31,15 +31,14 @@ export default async function ContactPage() {
             </p>
           </div>
         </div>
-        <div className="mt-10">
-          <ul
-            role="list"
-            className="mt-2 grid grid-cols-1 gap-4 sm:grid-cols-2"
-          >
+        <div className="mt-6">
+          <ul role="list" className="grid grid-cols-1 gap-4 sm:grid-cols-2">
             {social.map((item, index) => (
               <li key={index}>
-                <button
-                  type="button"
+                <a
+                  href={item.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className="shadow-xs group flex w-full items-center justify-between space-x-3 rounded-full border border-zinc-600/20 px-4 py-2 text-left hover:bg-zinc-200/40 dark:border-zinc-700/40 dark:hover:bg-zinc-500/10"
                 >
                   <span className="flex min-w-0 flex-1 items-center space-x-4">
@@ -47,7 +46,7 @@ export default async function ContactPage() {
                       <item.icon className="size-8 text-zinc-600 dark:text-zinc-400" />
                     </span>
                     <span className="block min-w-0 flex-1">
-                      <span className="block truncate text-sm font-medium text-zinc-950 dark:text-white">
+                      <span className="block truncate text-sm font-semibold text-zinc-600 dark:text-zinc-400">
                         {item.name}
                       </span>
                       <span className="block truncate text-sm font-medium text-zinc-600 dark:text-zinc-400">
@@ -61,7 +60,7 @@ export default async function ContactPage() {
                       className="size-5 text-gray-400 dark:text-gray-400/10"
                     />
                   </span>
-                </button>
+                </a>
               </li>
             ))}
           </ul>
