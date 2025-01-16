350807b9aba88f4f05e4b07589d2953ec61def5e
Tim Baz <timtb.dev@gmail.com>
2025-01-16 06:18:53 -0800

    refactor: update import paths for helper functions to improve consistency

--- components/ui/avatar.tsx
+++ components/ui/avatar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { cn } from "@/lib/helpers";
+import { cn } from "@/utils/helpers";
 import * as AvatarPrimitive from "@radix-ui/react-avatar";
 import * as React from "react";
 
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/helpers";
+import { cn } from "@/utils/helpers";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 import * as React from "react";
