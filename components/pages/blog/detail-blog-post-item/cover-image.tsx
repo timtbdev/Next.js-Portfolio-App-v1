8a16aa6204ef205ae90226d98c9c020b9020b654
Tim Baz <timtb.dev@gmail.com>
2025-01-31 20:59:15 -0800

    feat: add 'Back' label to cover image for improved navigation in blog post detail view

--- components/pages/blog/detail-blog-post-item/cover-image.tsx
+++ components/pages/blog/detail-blog-post-item/cover-image.tsx
@@ -17,6 +17,9 @@ const CoverImage: FC<Props> = ({ imageUrl }) => {
         className="h-96 w-full object-cover lg:rounded-t-[0.62rem]"
         placeholder={`data:image/svg+xml;base64,${toBase64(shimmer(512, 288))}`}
       />
+      <span className="absolute left-[20px] top-[20px] rounded-full bg-white px-[4px] text-xs font-semibold text-gray-500 shadow-sm ring-1 ring-black/5">
+        Back
+      </span>
     </div>
   );
 };
