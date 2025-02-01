c6175ec695e71b8c43c1290b65de9ca7eff8110a
Tim Baz <timtb.dev@gmail.com>
2025-02-01 07:34:29 -0800

    feat: enhance blog post item with clickable title and improved Read More button styling

--- components/pages/blog/single-blog-post-item/main.tsx
+++ components/pages/blog/single-blog-post-item/main.tsx
@@ -17,12 +17,13 @@ interface Props {
 
 const BlogPostItem: FC<Props> = ({ post }) => {
   const readTime = readingTime(post.content, { wordsPerMinute: 100 }).minutes;
+  const url = `/blog/post/${post.slug}`;
   return (
     <Card>
       <article className="relative isolate flex flex-col gap-2 px-0 py-0 sm:gap-8 sm:px-10 sm:py-6 lg:flex-row">
         <ImageSection post={post} />
-        <div className="group relative max-w-xl px-5 pb-6 pt-3 sm:px-0 sm:py-0">
-          <Title title={post.data.title} />
+        <div className="relative max-w-xl px-5 pb-6 pt-3 sm:px-0 sm:py-0">
+          <Title title={post.data.title} url={url} />
           <Tags tags={post.data.tags} />
           <div className="mt-3 flex items-center gap-x-3">
             <Date date={post.data.date} />
@@ -31,7 +32,7 @@ const BlogPostItem: FC<Props> = ({ post }) => {
           </div>
           <Description description={post.data.description} />
           <div className="relative mt-3 max-w-xl justify-start">
-            <ReadMoreButton url={`/blog/post/${post.slug}`} />
+            <ReadMoreButton url={url} />
           </div>
         </div>
       </article>
--- components/pages/blog/single-blog-post-item/title.tsx
+++ components/pages/blog/single-blog-post-item/title.tsx
@@ -1,12 +1,17 @@
-import exp from "constants";
+import { Link } from "next-view-transitions";
 import { FC } from "react";
 
 interface Props {
   title: string;
+  url: string;
 }
 
-const Title: FC<Props> = ({ title }) => {
-  return <h3 className="line-clamp-2 text-pretty">{title}</h3>;
+const Title: FC<Props> = ({ title, url }) => {
+  return (
+    <Link href={url}>
+      <h3 className="line-clamp-2 text-pretty hover:underline">{title}</h3>
+    </Link>
+  );
 };
 
 export default Title;
