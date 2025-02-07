df2c2683aca3c8fcfeb3d4897d9ba151b4398acd
Tim Baz <timtb.dev@gmail.com>
2025-02-07 10:17:29 -0800

    fix: remove bottom margin from Avatar component for improved layout consistency

--- components/pages/home/avatar.tsx
+++ components/pages/home/avatar.tsx
@@ -12,7 +12,7 @@ interface Props {
 
 const Avatar: FC<Props> = ({ image, initials }) => {
   return (
-    <AvatarWrapper className="mx-auto mb-2 h-32 w-32 rounded-full shadow-md ring-1 ring-gray-300 dark:ring-zinc-700">
+    <AvatarWrapper className="mx-auto h-32 w-32 rounded-full shadow-md ring-1 ring-gray-300 dark:ring-zinc-700">
       <AvatarImage src={image} alt={`${initials}'s avatar`} />
       <AvatarFallback>{initials}</AvatarFallback>
     </AvatarWrapper>
