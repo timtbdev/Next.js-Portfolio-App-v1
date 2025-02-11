7e60b3c3041529b4cf169c22ab40af3d045d880e
Tim Baz <timtb.dev@gmail.com>
2025-02-10 17:08:54 -0800

    feat: enhance header styling with backdrop blur effect for improved visual clarity

--- components/header/main.tsx
+++ components/header/main.tsx
@@ -10,7 +10,7 @@ const Header = () => {
   const path = usePathname();
   const currentPath = `/${path.split("/")[1]}`;
   return (
-    <header className="sticky top-0 z-10 border-b-[1.2px] border-zinc-600/20 bg-white shadow-sm shadow-zinc-800/5 dark:border-zinc-700/40 dark:bg-zinc-900">
+    <header className="sticky top-0 z-10 border-b-[1.2px] border-zinc-600/20 bg-white/30 shadow-sm shadow-zinc-800/5 backdrop-blur-lg dark:border-zinc-700/40 dark:bg-zinc-900/80">
       <nav
         aria-label="Navigation"
         className="mx-auto flex max-w-4xl items-center justify-between px-6 py-2.5 md:py-5"
