7f18e7a4cb6057a1255a719efb857c4b45ff2346
Tim Baz <timtb.dev@gmail.com>
2025-03-07 14:53:29 -0800

    refactor: simplify DialogContent className by removing unnecessary animations

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -36,7 +36,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-right-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-right-1/2 data-[state=open]:slide-in-from-top-[48%] fixed left-[50%] z-50 grid w-full max-w-lg -translate-x-1/2 gap-4 border p-6 shadow-lg duration-200 sm:rounded-lg",
+        "bg-background fixed left-[50%] z-50 grid w-full max-w-lg -translate-x-1/2 gap-4 border p-6 shadow-lg duration-200 sm:rounded-lg",
         className,
       )}
       {...props}
